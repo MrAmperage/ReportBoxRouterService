de132ce1efc32a9380cbbb9528f3d4f8fedd7a24
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-04-26 11:20:03 +0700

    Update to 0.0.0.2h

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/MrAmperage/GoWebStruct/ApplicationCore"
 	"github.com/MrAmperage/ReportBoxRouterService/Controllers"
@@ -12,17 +13,20 @@ func main() {
 	ErrorInitService := RouterService.Init()
 	if ErrorInitService != nil {
 		fmt.Println(ErrorInitService)
+		os.Exit(0)
 	}
 	Controllers.InitControllers(RouterService)
 	ErrorStartService := RouterService.StartRabbitMQ()
 	if ErrorStartService != nil {
 
 		fmt.Println(ErrorStartService)
+		os.Exit(0)
 	}
 
 	ErrorStartWebServer := RouterService.StartWebServer()
 	if ErrorStartWebServer != nil {
 		fmt.Println(ErrorStartWebServer)
+		os.Exit(0)
 	}
 
 }
