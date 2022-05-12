733897a1f5fc240ac56d1d6bbce1587fdf023518
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-05-12 10:39:45 +0700

    Update to 0.0.0.2m

--- Controllers/InitControllers.go
+++ Controllers/InitControllers.go
@@ -8,5 +8,5 @@ func InitControllers(ApplicationCore *ApplicationCore.ApplicationCore) {
 	ApplicationCore.WebCore.Router.HandleFunc("/api/authentication", ApplicationCore.WebCore.Middleware.HandlerMiddleware(AuthenticationController, &ApplicationCore.WebCore)).Methods("POST")
 	ApplicationCore.WebCore.Router.HandleFunc("/api/users", ApplicationCore.WebCore.Middleware.HandlerMiddleware(AddUser, &ApplicationCore.WebCore)).Methods("POST")
 	ApplicationCore.WebCore.Router.HandleFunc("/api/users/{username}", ApplicationCore.WebCore.Middleware.HandlerMiddleware(DeleteUser, &ApplicationCore.WebCore)).Methods("DELETE")
-
+	ApplicationCore.WebCore.Router.HandleFunc("/api/users/{username}", ApplicationCore.WebCore.Middleware.HandlerMiddleware(EditUser, &ApplicationCore.WebCore)).Methods("PATCH")
 }
--- Controllers/UsersController.go
+++ Controllers/UsersController.go
@@ -64,3 +64,32 @@ func DeleteUser(ResponseWriter http.ResponseWriter, Request *http.Request, WebCo
 	}
 	return string(RabbitMessage.Body), Error
 }
+
+func EditUser(ResponseWriter http.ResponseWriter, Request *http.Request, WebCoreObject *WebCore.WebCore) (Data interface{}, Error error) {
+	NewCorrelationId := uuid.NewString()
+
+	ByteBody, Error := ioutil.ReadAll(Request.Body)
+	if Error != nil {
+		return
+	}
+	ReplySubscribe, Error := WebCoreObject.RabbitMQ.RabbitMQChanel.GetSubscribeByQueueName("amq.rabbitmq.reply-to")
+	if Error != nil {
+		return
+	}
+
+	Error = WebCoreObject.RabbitMQ.RabbitMQChanel.Chanel.Publish("RportBoxExchange", "Users", false, false, amqp.Publishing{
+		Type:          "PATCH",
+		ContentType:   "application/json",
+		Body:          ByteBody,
+		ReplyTo:       `amq.rabbitmq.reply-to`,
+		CorrelationId: NewCorrelationId,
+	})
+	if Error != nil {
+		return
+	}
+	RabbitMessage, Error := ReplySubscribe.GetMessageByCorrelationId(NewCorrelationId)
+	if Error != nil {
+		return
+	}
+	return string(RabbitMessage.Body), Error
+}
