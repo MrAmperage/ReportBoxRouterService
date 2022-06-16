f1e3295738a07d77efde338ea1b799ee89f11606
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-06-16 10:40:11 +0700

    Update to 0.0.0.3c

--- Controllers/RootController.go
+++ Controllers/RootController.go
@@ -3,19 +3,12 @@ package Controllers
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 
 	"github.com/MrAmperage/GoWebStruct/WebCore"
 )
 
 func RootController(ResponseWriter http.ResponseWriter, Request *http.Request, WebCore *WebCore.WebCore) (Data interface{}, Error error) {
-	Template, Error := template.ParseFiles(fmt.Sprintf("%s/Pages/Index.html", WebCore.FileServer.StaticDirectory))
-	if Error != nil {
-		return
-	}
-	Error = Template.Execute(ResponseWriter, nil)
-	if Error != nil {
-		return
-	}
+	http.ServeFile(ResponseWriter, Request, fmt.Sprintf("%s/Pages/Index.html", WebCore.FileServer.StaticDirectory))
+
 	return
 }
