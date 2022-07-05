fa576f38921ab89e10a3e59a150ad0dd4b057745
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-05 09:33:16 +0700

    Update to 0.0.6a

--- Models/LeftMenu.go
+++ Models/LeftMenu.go
@@ -1,10 +1,7 @@
 package Models
 
-import "github.com/google/uuid"
-
 type LeftMenu struct {
-	Caption   string    `gorm:"primary_key;not null;caption"`
-	Id        string    `gorm:"not null;id"`
-	TopMenuId string    `gorm:"not null;top_menu_id"`
-	SchemeId  uuid.UUID `gorm:"type:uuid;scheme_id"`
+	Caption   string `gorm:"primary_key;not null;caption"`
+	Id        string `gorm:"not null;id"`
+	TopMenuId string `gorm:"not null;top_menu_id"`
 }
