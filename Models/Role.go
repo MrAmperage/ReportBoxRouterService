235449762023e9b938be76ee269a7dde7bd9a2a6
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-07 14:29:44 +0700

    Update to 0.0.6i

--- Models/Role.go
+++ Models/Role.go
@@ -3,7 +3,6 @@ package Models
 import "github.com/google/uuid"
 
 type Role struct {
-	Id          uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
-	Rolename    string    `gorm:"not null;rolename"`
-	Description string    `gorm:"not null;description"`
+	Id       uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	Rolename string    `gorm:"not null;rolename"`
 }
