efe7d34db565fe9d5505bbd015eb8bdd6259c435
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-11 09:19:31 +0700

    Update to 0.0.6j

--- Models/User.go
+++ Models/User.go
@@ -1,12 +1,18 @@
 package Models
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type User struct {
-	Id       uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
-	Username string    `gorm:"primaryKey;not null;username"`
-	Password string    `gorm:"not null;password"`
-	Enabled  bool      `gorm:"not null;enabled"`
-	RoleId   string    `gorm:"not null;rolename"`
-	Role     Role      `gorm:"foreignkey:Id;references:role_id"`
+	Id        uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	Username  string    `gorm:"primaryKey;not null;username"`
+	Password  string    `gorm:"not null;password"`
+	Enabled   bool      `gorm:"not null;enabled"`
+	RoleId    string    `gorm:"not null;rolename"`
+	Role      Role      `gorm:"foreignkey:Id;references:role_id"`
+	StartDate time.Time `gorm:"not null;start_date"`
+	EndDate   time.Time `gorm:"not null;end_date"`
 }
