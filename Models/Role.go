f882849ea55e25e7e6a473347a23db18beef9a1f
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-18 09:43:15 +0700

    Update to 0.0.6o

--- Models/Role.go
+++ Models/Role.go
@@ -1,8 +1,13 @@
 package Models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+)
 
 type Role struct {
-	Id       uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
-	Rolename string    `gorm:"not null;rolename"`
+	Id           uuid.UUID      `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	Rolename     string         `gorm:"not null;rolename"`
+	MenusAccess  pq.StringArray `gorm:"type:varchar[];default:array[]::varchar[];menus_access"`
+	GroupsAccess pq.StringArray `gorm:"type:varchar[];default:array[]::varchar[];groups_access"`
 }
--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/jackc/pgx/v4 v4.16.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.6
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/text v0.3.7 // indirect
