2af56965ead5f3caa3e4e3f247655d952464f662
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-21 11:10:26 +0700

    Update to 0.0.6v

--- Models/Organization.go
+++ Models/Organization.go
@@ -6,5 +6,4 @@ type Organization struct {
 	Id       uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
 	Caption  string    `gorm:"not null;caption"`
 	Internal bool      `gorm:"not null;internal"`
-	Areas    []Area
 }
